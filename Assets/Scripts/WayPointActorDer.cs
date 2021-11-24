dcd9363da4836ff8915053fd3afc090803394ce0
Diego Andrés Moreno Molina <53064110+D-AndresMoreno@users.noreply.github.com>
2021-11-24 17:18:57 -0600

    Implemented 5 robots correctly (no loops)

--- Assets/Scripts/WayPointActorDer.cs
+++ Assets/Scripts/WayPointActorDer.cs
@@ -19,6 +19,8 @@ public class WayPointActorDer : MonoBehaviour
     public GameObject cajaEntregada2;
     public GameObject cajaEntregada3;
     
+
+    bool regreso = false;
     // Start is called before the first frame update
     void Start()
     {
